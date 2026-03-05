c28f51321fe4d7d0ca9ae7f7bd4798d56bb35750
allisonua-12 <allisonua@gmail.com>
2026-03-05 09:48:36 -0500

    cis4340a.java file

---
+++ cis4340a.java
@@ -0,0 +1,7 @@
+public class cis4340a {
+    public static void main(String[] args) {
+        String name = "Allison Walker";
+        System.out.println("Hello CIS4340!");
+        System.out.println("My name is " + name);
+    }
+}
