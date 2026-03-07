89c33ab7322fe87b8571560563e12261d7caeab1
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:41:04 -0500

    fixed R06 MET01 J.java
    compliant

--- R06 MET01 J.java
+++ R06 MET01 J.java
@@ -1,11 +1,12 @@
 public class R06_MET01_J {
     public static int getAbsAdd(int x, int y) {
-        assert x != Integer.MIN_VALUE;
-        assert y != Integer.MIN_VALUE;
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();}
         int absX = Math.abs(x);
         int absY = Math.abs(y);
-        assert (absX <= Integer.MAX_VALUE - absY);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();}
         return absX + absY;}
     public static void main(String[] args) {
-        System.out.println("Noncompliant MET01-J example");}
+        System.out.println("Compliant MET01-J example");}
 }
