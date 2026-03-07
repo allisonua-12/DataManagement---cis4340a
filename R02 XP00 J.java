a47595c863008982901c162cd2e7d65f39dacb99
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:29:42 -0500

    fixed R02 XP00 J.java
    compliant version

--- R02 XP00 J.java
+++ R02 XP00 J.java
@@ -2,8 +2,10 @@ import java.io.File;
 public class R02_XP00_J {
     public static void deleteFile() {
         File someFile = new File("someFileName.txt");
-       // Do something with someFile
-        someFile.delete();}
+        // Do something with someFile
+        if (!someFile.delete()) {
+            System.out.println("Failed to delete the file.");}
+    }
     public static void main(String[] args) {
         deleteFile();}
 }
