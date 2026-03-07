73c876140c8c3dade46222a8cf79944807356eb4
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:37:15 -0500

    fixed R04 STR03 J.java
    compliant

--- R04 STR03 J.java
+++ R04 STR03 J.java
@@ -2,9 +2,9 @@ import java.math.BigInteger;
 public class R04_STR03_J {
     public static void main(String[] args) {
         BigInteger x = new BigInteger("530500452766");
-        byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray);
-        byteArray = s.getBytes();
-        x = new BigInteger(byteArray);
-        System.out.println("Noncompliant STR03-J example: " + x);}
+        String s = x.toString(); // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
+        System.out.println("Compliant STR03-J example: " + x);}
 }
