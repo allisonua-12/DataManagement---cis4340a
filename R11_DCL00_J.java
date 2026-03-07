42368ef0553bc7c2f354a555c58dbdb257e50761
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:58:21 -0500

    fixed R11_DCL00_J.java
    compliant

--- R11_DCL00_J.java
+++ R11_DCL00_J.java
@@ -1,7 +1,10 @@
 public class R11_DCL00_J {
-    static int value = Helper.number + 1;
+    static int value = 1;
     public static void main(String[] args) {
-        System.out.println("Value: " + value);}
+        System.out.println("Value: " + value);
+        System.out.println("Number: " + Helper.getNumber());}
 }
 class Helper {
-    static int number = R11_DCL00_J.value + 1;}
+    static int getNumber() {
+        return R11_DCL00_J.value + 1;}
+}
