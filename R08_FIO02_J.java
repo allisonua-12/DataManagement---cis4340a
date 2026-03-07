5a5bf84d72be4b4e825fa835ab21352a8486c3d1
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:49:03 -0500

    fixed R08_FIO02_J.java
    compliant

--- R08_FIO02_J.java
+++ R08_FIO02_J.java
@@ -1,9 +1,13 @@
 import java.io.FileReader;
 import java.io.IOException;
 public class R08_FIO02_J {
-    public static void main(String[] args) throws IOException {
-        FileReader reader = new FileReader("test.txt");
-        int data = reader.read();
-        System.out.println("First character: " + (char) data);
-        reader.close();}
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("test.txt");
+            int data = reader.read();
+            System.out.println("First character: " + (char) data);
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("File error: " + e.getMessage());}
+    }
 }
