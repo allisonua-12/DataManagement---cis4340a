bb656a50c2e2c75713a37c21facebea4ea7c6d6a
allisonua-12 <allisonua@gmail.com>
2026-03-07 08:34:25 -0500

    fixed R03 NUM03 J.java
    compliant

--- R03 NUM03 J.java
+++ R03 NUM03 J.java
@@ -1,8 +1,8 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 public class R03_NUM03_J {
-    public static int getInteger(DataInputStream is) throws IOException {
-        return is.readInt();}
+    public static long getInteger(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL;}
     public static void main(String[] args) {
-        System.out.println("Noncompliant NUM03-J example");}
+        System.out.println("Compliant NUM03-J example");}
 }
